17a3aa82e3e5ddbd8c3bf99ece449a84f8668002
Takahashi Takumu <takumu.0213@icloud.com>
2020-02-16 15:24:26 +0900

    [Finish] 3-7

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -4,7 +4,6 @@ class TodolistsController < ApplicationController
     # View へ渡すためのインスタンス変数に空のモデルオブジェクトを生成する。
     @list = List.new
   end
-
   def create
     #ストロングパラメーターを使用
     list = List.new(list_params)
@@ -13,17 +12,23 @@ class TodolistsController < ApplicationController
     #詳細画面へリダイレクト
     redirect_to todolist_path(list.id)
   end
-
   def index
     @lists = List.all
   end
-
   def show
     @list = List.find(params[:id])
   end
+  def edit
+    @list = List.find(params[:id])
+  end
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to todolist_path(list.id)
+  end
+
 
   private
-
   def list_params
     params.require(:list).permit(:title, :body)
   end
